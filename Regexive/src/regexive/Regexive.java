25184f32e6eeb069c335cb331277ae3a9d9cde0a
JeffMendez <mjeffryemanuel@gmail.com>
2021-02-13 21:02:11 -0600

    Creacion proyecto y vista principal

---
+++ Regexive/src/regexive/Regexive.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regexive;
+
+import vistas.Menu;
+/**
+ *
+ * @author Jeffry
+ */
+public class Regexive {
+
+    static public Menu menu_principal;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        menu_principal = new Menu();
+        menu_principal.setVisible(true);
+    }
+    
+}
